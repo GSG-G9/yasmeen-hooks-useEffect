1ea77f9895d1e8ed68de065b8fd8dbb08aac45a4
yasmeenattallah <yattallah7@gmail.com>
2021-01-19 17:08:47 +0200

    exercise_1 solution

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  ExerciseOne from "./components/exercise_1";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ExerciseOne/>
+    </div>
+  );
+}
+
+export default App;
